e5339b50754fb011d3b83abcf6395d9d0f2fcfd9
Marcin Ignac <marcin.ignac@gmail.com>
2015-11-26 23:44:42 +0000

    loadJSON added documentation

--- loadJSON.js
+++ loadJSON.js
@@ -1,7 +1,14 @@
 var loadText = require('./loadText');
 
-function loadJSON(url, callback) {
-  loadText(url, function(err, data) {
+/**
+ * Loads JSON data
+ * @param {String} file - url addess (Browser) or file path (Plask)
+ * @param {Function} callback - function(err, json) { }
+ * @param {Error} callback.err - error if any or null
+ * @param {String} callback.json - loaded JSON data
+ */
+function loadJSON(file, callback) {
+  loadText(file, function(err, data) {
     if (err) {
       callback(err, null);
     }
