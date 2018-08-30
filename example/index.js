0d5e5b6cf086cfd96573815acd8d7fcc2b54fb2b
vorg <marcin.ignac@gmail.com>
2018-08-30 14:49:29 +0100

    Update example with async

--- example/index.js
+++ example/index.js
@@ -1,64 +1,149 @@
-var gl = require('pex-gl')(512, 512)
-var regl = require('regl')(gl)
-// var loadimage = require('../loadimage')
-// loadimage('pex-logo.png', (err, img) => {
+const hdom = require('@thi.ng/hdom')
+const io = require('..')
 
-var load = require('../load')
-
-var resources = {
-  img: { image: 'pex-logo.png' }
+var state = {
+  callback: {
+    text: null,
+    json: null,
+    image: null,
+    binary: null
+  },
+  callbackBatch: {
+    text: null,
+    json: null,
+    image: null,
+    binary: null
+  },
+  async: {
+    text: null,
+    json: null,
+    image: null,
+    binary: null
+  },
+  asyncBatch: {
+    text: null,
+    json: null,
+    image: null,
+    binary: null
+  }
 }
 
-load(resources, (err, res) => {
-  if (err) {
-    console.log(err)
-    return
-  }
-  var img = res.img
+const appStyle = {
+  'font-family': 'sans-serif',
+  'line-height': '125%'
+}
 
-  console.log('img w:' + img.width + ' h:' + img.height)
+const techniqueStyle = {
+  width: '49%',
+  float: 'left',
+  'margin-top': '2em'
+}
 
-  var tex = regl.texture(img)
+const itemHeaderStyle = {
+  margin: '1em 0 0 0',
+  padding: 0
+}
 
-  var drawImage = regl({
-    attributes: {
-      position: [[-1, -1], [1, -1], [1, 1], [-1, 1]],
-      // flip UV coords to match WebGL
-      texCoord: [[0, 1], [1, 1], [1, 0], [0, 0]]
-    },
-    elements: [[0, 1, 2], [0, 2, 3]],
-    uniforms: {
-      image: regl.prop('image')
-    },
-    vert: `
-      #ifdef GL_ES
-      precision mediump float;
-      #endif
-      attribute vec2 position;
-      attribute vec2 texCoord;
-      varying vec2 vTexCoord;
-      void main() {
-        gl_Position = vec4(position, 0.0, 1.0);
-        vTexCoord = texCoord;
-      }
-    `,
-    frag: `
-      #ifdef GL_ES
-      precision mediump float;
-      #endif
-      varying vec2 vTexCoord;
-      uniform sampler2D image;
-      void main() {
-        gl_FragColor = texture2D(image, vTexCoord);
-      }
-    `
-  })
-
-  regl.frame(() => {
-    regl.clear({
-      color: [1, 0, 0, 1],
-      depth: 1
+const loadingStatus = () => {
+  const panels = Object.keys(state).map((technique) => {
+    const items = Object.keys(state[technique]).map((itemName) => {
+      let value = state[technique][itemName]
+      if (!value) value = 'loading...'
+      if (value.color) value = JSON.stringify(value)
+      if (value instanceof window.Image) value = ['img', { src: value.src }]
+      if (value instanceof ArrayBuffer) value = `ArrayBuffer ${value.byteLength} bytes`
+      return ['div',
+        ['h4', { style: itemHeaderStyle }, itemName],
+        value
+      ]
     })
-    drawImage({image: tex})
+    return ['div',
+      { style: techniqueStyle },
+      ['h2', technique],
+      items
+    ]
   })
-})
+  return ['div', panels
+  ]
+}
+
+const app = () => {
+  return () => {
+    return ['div#app', { style: appStyle }, loadingStatus()]
+  }
+}
+
+hdom.start(document.body, app())
+
+// callback
+
+function loadCallback () {
+  io.loadText('assets/hello.txt', (err, text) => {
+    if (err) return console.log(err)
+    state.callback.text = text
+  })
+
+  io.loadJSON('assets/color.json', (err, json) => {
+    if (err) return console.log(err)
+    state.callback.json = json
+  })
+
+  io.loadImage('assets/pex.png', (err, image) => {
+    if (err) return console.log(err)
+    state.callback.image = image
+  })
+
+  io.loadBinary('assets/data.bin', (err, data) => {
+    if (err) return console.log(err)
+    state.callback.binary = data
+  })
+}
+
+loadCallback()
+
+// callback batch
+
+function loadCallbackBatch () {
+  io.load({
+    hello: { text: 'assets/hello.txt' },
+    color: { json: 'assets/color.json' },
+    pex: { image: 'assets/pex.png' },
+    data: { binary: 'assets/data.bin' }
+  }, (err, res) => {
+    if (err) return console.log(err)
+    state.callbackBatch.text = res.hello
+    state.callbackBatch.json = res.color
+    state.callbackBatch.image = res.pex
+    state.callbackBatch.binary = res.data
+  })
+}
+
+loadCallbackBatch()
+
+// async
+
+async function loadAsync () {
+  state.async.text = await io.loadText('assets/hello.txt')
+  state.async.json = await io.loadJSON('assets/color.json')
+  state.async.image = await io.loadImage('assets/pex.png')
+  state.async.binary = await io.loadBinary('assets/color.json')
+}
+
+loadAsync()
+
+// async batch
+
+async function loadAsyncBatch () {
+  const res = await io.load({
+    hello: { text: 'assets/hello.txt' },
+    color: { json: 'assets/color.json' },
+    pex: { image: 'assets/pex.png' },
+    data: { binary: 'assets/data.bin' }
+  })
+  state.asyncBatch.text = res.hello
+  state.asyncBatch.json = res.color
+  state.asyncBatch.image = res.pex
+  state.asyncBatch.binary = res.data
+}
+
+loadAsyncBatch()
